fa22f0d89bd3d74f90cd51a90197d3961b423650
Dzmitry Shabailau <budulayreturns@gmail.com>
2020-12-13 16:16:48 +0300

    Initial Commit

---
+++ main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  main
+//
+//  Created by Dzmitry on 13.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
